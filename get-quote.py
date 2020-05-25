77c65727ecf560b61eaa4df7a4342adc8736248e
ChunkyWasabi <47772244+ChunkyWasabi@users.noreply.github.com>
2020-05-25 13:02:51 -0700

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -5,8 +5,8 @@ def main():
   f.close()
 
   last = 13
-  rnd = random.randint(o, last)
-  print(quotes[13])
+  rnd = random.randint(0, last)
+  print(quotes[rnd])
 
 if __name__== "__main__":
   main()
